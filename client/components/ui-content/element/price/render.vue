bbd6185b7e2990f451c924e17f23d2bb959064fa
Jeferson Inácio <jeferson.i.silva@gmail.com>
2021-11-05 19:36:49 -0300

    admin card and modal

--- client/components/ui-content/element/price/render.vue
+++ client/components/ui-content/element/price/render.vue
@@ -37,14 +37,14 @@ export default {
 </script>
 
 <style>
-.card {
+/* .card {
     position: relative;
     background: #fff;
     box-shadow: 0 0 15px rgba(0, 0, 0, .1);
     overflow: hidden;
     padding: 20px;
     border: none
-}
+} */
 
 .ribon {
     width: 30px;
